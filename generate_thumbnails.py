f0717d9961e339be0b39185c32086bf2d3a2adb0
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-06-05 16:28:13 +0100

    Determine and use source image file type

--- generate_thumbnails.py
+++ generate_thumbnails.py
@@ -8,7 +8,7 @@ from retrying import retry
 
 THUMBNAIL_SIZE = int(os.getenv('THUMBNAIL_SIZE', '128'))
 THUMBNAIL_MAX_DIM = THUMBNAIL_SIZE, THUMBNAIL_SIZE
-THUMBNAIL_SUFFIX = f'_thumb{THUMBNAIL_SIZE}.jpg'
+THUMBNAIL_SUFFIX = f'_thumb{THUMBNAIL_SIZE}'
 
 
 def main(event, context):
@@ -26,23 +26,25 @@ def main(event, context):
     bucket = client.get_bucket(event['bucket'])
     with tempfile.NamedTemporaryFile() as temp_image_file, tempfile.NamedTemporaryFile() as temp_thumb_file:
         get_image_file(event, temp_image_file.name, bucket)
-        generate_thumbnail(temp_image_file.name, temp_thumb_file.name)
-        upload_thumbnail_to_bucket(bucket, event, temp_thumb_file)
+        image_format = generate_thumbnail(temp_image_file.name, temp_thumb_file.name)
+        upload_thumbnail_to_bucket(bucket, temp_thumb_file, get_thumbnail_name(event['name'], image_format))
 
 
-def get_thumbnail_name(image_name):
-    return f'{Path(image_name).stem}{THUMBNAIL_SUFFIX}'
+def get_thumbnail_name(image_name, image_format):
+    return f'{Path(image_name).stem}{THUMBNAIL_SUFFIX}.{image_format}'
 
 
 def generate_thumbnail(image_file_name, thumbnail_file_name):
     image = Image.open(image_file_name)
-    image.thumbnail(THUMBNAIL_MAX_DIM)
-    image.save(thumbnail_file_name, format='JPEG')
+    image_format = image.format
+    image.thumbnail(THUMBNAIL_MAX_DIM, Image.ANTIALIAS)
+    image.save(thumbnail_file_name, format=image_format)
+    return image_format
 
 
 @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000)
-def upload_thumbnail_to_bucket(bucket, event, temp_thumb_file):
-    bucket.blob(get_thumbnail_name(event['name'])).upload_from_filename(temp_thumb_file.name, content_type='image/jpeg')
+def upload_thumbnail_to_bucket(bucket, temp_thumb_file, thumbnail_filename):
+    bucket.blob(thumbnail_filename).upload_from_filename(temp_thumb_file.name, content_type='image/jpeg')
 
 
 @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000)
